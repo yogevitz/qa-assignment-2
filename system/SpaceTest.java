7b0bc6d1956bb8124adcc658cc8e9b855317e304
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-27 18:13:25 +0200

    feature: initialized space tests

---
+++ system/SpaceTest.java
@@ -0,0 +1,36 @@
+package system;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpaceTest {
+
+    FileSystem fileSystem;
+
+    @Before
+    public void initialize() {
+        fileSystem = new FileSystem(1000);
+    }
+
+    @Test
+    public void alloc() {
+    }
+
+    @Test
+    public void allocException() {
+    }
+
+    @Test
+    public void dealloc() {
+    }
+
+    @Test
+    public void countFreeSpace() {
+    }
+
+    @Test
+    public void getAlloc() {
+    }
+}
