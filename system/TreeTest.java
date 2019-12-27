702a009d5a58f27c809aea982a4d879b8cdc5e92
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-27 18:14:05 +0200

    feature: initialized tree tests

---
+++ system/TreeTest.java
@@ -0,0 +1,16 @@
+package system;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+    @Test
+    public void getPath() {
+    }
+
+    @Test
+    public void getChildByName() {
+    }
+}
