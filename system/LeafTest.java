90df987ad59ae0303dd1fe9ed1b7ae7f974367cc
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-27 18:02:56 +0200

    feature: added create leaf test

---
+++ system/LeafTest.java
@@ -0,0 +1,27 @@
+package system;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeafTest {
+
+    FileSystem fileSystem = new FileSystem(100);
+
+    @Before
+    public void initialize() {
+        fileSystem = new FileSystem(1000);
+    }
+
+    @Test
+    public void createLeaf() {
+        Leaf leaf = null;
+        try {
+            leaf = new Leaf("yogev", 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(leaf);
+    }
+}
