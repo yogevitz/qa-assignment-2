49b9a6e7b01c58747063abb273e14d6d968d67e2
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-28 18:05:03 +0200

    feature: added setting for tree tests

--- system/TreeTest.java
+++ system/TreeTest.java
@@ -1,10 +1,24 @@
 package system;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TreeTest {
+
+    FileSystemStub fileSystemStub;
+
+    @Before
+    public void initialize() {
+        fileSystemStub = new FileSystemStub(1000);
+    }
+
+    @After
+    public void erase() {
+        fileSystemStub = null;
+    }
 
     @Test
     public void getPath() {
