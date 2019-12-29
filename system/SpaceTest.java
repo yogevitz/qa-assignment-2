68770541674464bc9d9e78167648cbda89796143
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-29 23:07:02 +0200

    feature: added space alloc test

--- system/SpaceTest.java
+++ system/SpaceTest.java
@@ -22,6 +22,21 @@ public class SpaceTest {
 
     @Test
     public void alloc() {
+        Space space;
+        Leaf leaf;
+        Leaf[] blocks;
+        String name = "yogev";
+        int size = 10;
+
+        try {
+            leaf = new Leaf(name, size);
+            space = new Space(size);
+            space.Alloc(size, leaf);
+            blocks = space.getAlloc();
+            for (int i = 0; i < leaf.allocations.length; i++) {
+                assertEquals(blocks[leaf.allocations[i]], leaf);
+            }
+        } catch (Exception e) {}
     }
 
     @Test
