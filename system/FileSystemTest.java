93f286548a64617d9fcedd41d49ca515e48c9d9f
liadber <liadbercovich@gmail.com>
2019-12-29 22:29:56 +0200

    fix: initialize(), createFileSystem() ,updateFileStorage(), updateFileSystemTree()

--- system/FileSystemTest.java
+++ system/FileSystemTest.java
@@ -6,6 +6,28 @@ import static org.junit.Assert.*;
 
 public class FileSystemTest {
 
+    FileSystem fs;
+    @Before
+    public void initialize() {
+        fs = new FileSystem(1000000);
+    }
+
+    @Test
+    public void createFileSystem(){
+        assertNotNull(fs);
+    }
+
+    @Test
+    public void updateFileStorage(){
+        assertNotNull(fs.fileStorage);
+    }
+
+    @Test
+    public void updateFileSystemTree(){
+        String [] root = {"root"};
+        assertNotNull(fs.DirExists(root));
+    }
+
     @Test
     public void dir() {
 
